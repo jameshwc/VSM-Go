180f22af34b2a79eaf5199da1a2f2bca3864982b
Vulpic <jameshwc@gmail.com>
2020-04-04 11:10:11 +0800

    HW1 init

---
+++ HW1/script/compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Put your commands below (can be multiple lines) to compile your program.
+# Here's an example which will compile all the .c files in current directory with gcc:
+gcc *.c
+
